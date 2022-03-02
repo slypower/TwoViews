cc05dced0892ec16ab5afc91f7f806e0ec448287
Vital <slyvitax@mail.ru>
2022-03-02 20:32:20 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Виталий Бобрик on 1.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
